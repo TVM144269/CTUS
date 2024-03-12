a6f0cf970d0a77838da86d5ca28bde6d81f0a9bb
TVM144269 <162600666+TVM144269@users.noreply.github.com>
2024-03-12 12:59:32 +0600

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 import streamlit
-streamlit.set_page_config("SMS TO US")
-streamlit.success("NAME :")
-name = streamlit.text_input(".")
-streamlit.success("মেইল :")
-email = streamlit.text_input("..")
-streamlit.success("SMS :")
-message = streamlit.text_area("...")
-ww = len(name) >= 1  and len(email) >= 1 and len(message) >= 1
-file = open("D BASE.txt", "a")
-
-
-if streamlit.button("SUBMIT>") and len(name) >= 1  and len(email) >= 1 and len(message) >= 1:
-    streamlit.success("MESSAGE টি গ্রহণ করা হয়েছে")
-    file.write(name+"  "+email+"   "+message)
-    file.close()
-elif ww == False:
-    streamlit.warning("")
+streamlit.set_page_config("CLOSEO")
+link = streamlit.text_input(" ")
+if streamlit.button("OPEN"):
+    streamlit.video(link)
 else:
-    streamlit.warning("")
+    streamlit.success(" ")
